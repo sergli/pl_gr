21336b1931a8553f9b375ac0403f1e8bea86a72d
Sergey Lisenkov <s.lisenkov@corp.badoo.com>
2021-06-22 18:05:03 +0300

    navbar

--- resources/views/foreigners/index.blade.php
+++ resources/views/foreigners/index.blade.php
@@ -9,6 +9,33 @@
     @endif
 </div>
 
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <div class="container">
+        <a class="navbar-brand mr-auto" href="#">PL Group</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register-user') }}">Register</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
+@yield('content')
+
 
 @section('main')
 <div class="row">
